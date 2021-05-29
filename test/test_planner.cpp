3d7392f4e704e77d14ec0aadc4bb7054e17a99e7
ilarioazzollini <ilario.azzollini92@gmail.com>
2021-05-29 14:30:56 +0200

    updated tests: unfeasible problem added

--- test/test_planner.cpp
+++ test/test_planner.cpp
@@ -53,6 +53,30 @@ TEST(test_planner, feasible_with_obstacles)
     EXPECT_EQ(plan[plan.size() - 1].this_point, goal_point);
 }
 
+TEST(test_planner, unfeasible_problem)
+{
+    Matrix occupancy_matrix = Matrix(5,5);
+    occupancy_matrix.fill(255);
+
+    MapCoord starting_point;
+    starting_point.row = 0;
+    starting_point.column = 0;
+
+    MapCoord goal_point;
+    goal_point.row = 4;
+    goal_point.column = 4;
+
+    // Wall encapsulating the goal point
+    occupancy_matrix.set(3,3,0);
+    occupancy_matrix.set(3,4,0);
+    occupancy_matrix.set(4,3,0);
+
+    AStar planner = AStar();
+    std::vector<Node> plan = planner.compute_plan(occupancy_matrix, starting_point, goal_point);
+
+    EXPECT_NE(plan[plan.size() - 1].this_point, goal_point);
+}
+
 
 int main(int argc, char **argv)
 {
